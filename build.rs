61eb96af31d94977bf558d04d32cbf41b293ecff
Isaac Elbaz <script3r@gmail.com>
2021-02-04 12:47:28 -0500

    Protocol organization.

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(),Box<dyn std::error::Error>> {
-   tonic_build::compile_protos("proto/himalayadb.proto")?;
+   tonic_build::compile_protos("proto/himalaya_core.proto")?;
+   tonic_build::compile_protos("proto/himalaya_master.proto")?;
+   tonic_build::compile_protos("proto/himalaya_worker.proto")?;
    Ok(())
 }
