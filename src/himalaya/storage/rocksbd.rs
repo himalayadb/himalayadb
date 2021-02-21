1a4efc6905fc39741cc7a3c4cb16baaea0117997
Spencer McCreary <spencer_mccreary@compulsivesoftware.com>
2021-02-21 15:31:06 -0500

    use bufmut put methods

--- src/himalaya/storage/rocksbd.rs
+++ src/himalaya/storage/rocksbd.rs
@@ -1,22 +1,23 @@
 use crate::storage::Error;
 use crate::storage::Error::{Configuration, DeleteError, GetError, PutError};
+use bytes::BufMut;
 use bytes::Bytes;
-use rocksdb::{DB, Options, MergeOperands};
-use std::path::Path;
+use rocksdb::{MergeOperands, Options, DB};
 use std::cmp::Ordering;
+use std::mem;
+use std::path::Path;
 
 pub struct RocksDb {
     rocks: DB,
 }
 
 impl RocksDb {
-    fn ts_aware_merge(new_key: &[u8],
-                      existing_val: Option<&[u8]>,
-                      operands: &mut MergeOperands) -> Option<Vec<u8>> {
-
-        let latest_inc_val: Option<&[u8]> = operands.max_by(|v1,v2| {
-            v1[0..8].cmp(&v2[0..8])
-        });
+    fn ts_aware_merge(
+        new_key: &[u8],
+        existing_val: Option<&[u8]>,
+        operands: &mut MergeOperands,
+    ) -> Option<Vec<u8>> {
+        let latest_inc_val: Option<&[u8]> = operands.max_by(|v1, v2| v1[0..8].cmp(&v2[0..8]));
 
         match (existing_val, latest_inc_val) {
             (Some(e), Some(l)) => {
@@ -25,22 +26,14 @@ impl RocksDb {
                 } else {
                     Some(e.to_vec())
                 }
-            },
-            (Some(e), None) => {
-                Some(e.to_vec())
-            },
-            (None, Some(l)) => {
-                Some(l.to_vec())
-            },
-            _ => {
-                None
             }
+            (Some(e), None) => Some(e.to_vec()),
+            (None, Some(l)) => Some(l.to_vec()),
+            _ => None,
         }
     }
 
-
     pub fn create<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
-
         let mut opts = Options::default();
         opts.create_if_missing(true);
         opts.set_merge_operator("ts_aware_merge", RocksDb::ts_aware_merge, None);
@@ -51,16 +44,11 @@ impl RocksDb {
     }
 
     pub(crate) fn put(&self, key: &[u8], value: &[u8], ts: i64) -> Result<(), Error> {
-        let mut wrapped_value = Vec::with_capacity(value.len() + 8);
-        let ts_bytes = ts.to_be_bytes();
+        let mut wrapped_value = Vec::with_capacity(value.len() + mem::size_of::<i64>());
+        wrapped_value.put_i64(ts);
+        wrapped_value.put_slice(value);
 
-        wrapped_value.append(&mut ts_bytes.to_vec());
-        for x in value {
-            wrapped_value.push(*x)
-        }
-
-        self
-            .rocks
+        self.rocks
             .merge(key, wrapped_value)
             .map_err(|e| PutError(From::from(e)))?;
         Ok(())
@@ -81,7 +69,6 @@ impl RocksDb {
     }
 }
 
-
 #[cfg(test)]
 mod test {
     use std::borrow::Borrow;
@@ -96,7 +83,8 @@ mod test {
         rocksdb.put("key".as_bytes(), "value2".as_bytes(), 2);
         rocksdb.put("key".as_bytes(), "value1".as_bytes(), 1);
 
-        let val = rocksdb.get("key".as_bytes())
+        let val = rocksdb
+            .get("key".as_bytes())
             .expect("expected to receive key")
             .expect("expected to receive value");
 
